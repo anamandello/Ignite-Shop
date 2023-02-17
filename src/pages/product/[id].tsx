63efbf6a748af3888304dbfbe8c4ddf8a1680e2a
Ana <ana.mandello@gmail.com>
2023-02-16 22:44:02 -0300

    .

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,8 +1,21 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/src/styles/pages/product"
 import { useRouter } from "next/router"
 
 export default function Product(){
   const { query } = useRouter()
   return(
-    <h1>Product {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageContainer>
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam repellat veritatis quis animi deserunt perspiciatis cumque molestiae obcaecati molestias esse consequatur temporibus, quisquam ratione in unde facere nesciunt! Accusamus, recusandae!</p>
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
